5daf1cd229c477ec9a39f264592a7cf9c403b90e
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-13 14:43:03 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200813142159) do
-
-  create_table "artists", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "songs", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "artist_id"
-    t.index ["artist_id"], name: "index_songs_on_artist_id"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
